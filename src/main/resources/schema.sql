c196a1c681f037106682051160faed2944449d39
Khaled Saleh <khaled@connou.app>
2023-05-10 17:07:12 +0200

    fixing syntax errors in schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS Content (
     id INTEGER AUTO_INCREMENT,
     title varchar(255) NOT NULL,
-    desc tect,
+    desc text,
     status varchar(20) NOT NULL,
     content_type varchar(50) NOT NULL,
-    date_created TIMESTAMP NOT NULL,f
+    date_created TIMESTAMP NOT NULL,
     date_updated TIMESTAMP,
     url varchar(255),
-    primary key (id),
-)
+    primary key (id)
+);
