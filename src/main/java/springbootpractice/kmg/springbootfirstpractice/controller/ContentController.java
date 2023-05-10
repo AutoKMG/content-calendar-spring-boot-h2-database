0d25372fdbdf14a8887798d974ffeadd1bb36c59
Khaled Saleh <khaled@connou.app>
2023-05-10 17:02:24 +0200

    adding not empty validator to the title

--- src/main/java/springbootpractice/kmg/springbootfirstpractice/controller/ContentController.java
+++ src/main/java/springbootpractice/kmg/springbootfirstpractice/controller/ContentController.java
@@ -1,5 +1,6 @@
 package springbootpractice.kmg.springbootfirstpractice.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -29,7 +30,7 @@ public class ContentController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    public void create(@RequestBody Content content){
+    public void create(@Valid @RequestBody Content content){
         repository.save(content);
     }
 
--- src/main/java/springbootpractice/kmg/springbootfirstpractice/model/Content.java
+++ src/main/java/springbootpractice/kmg/springbootfirstpractice/model/Content.java
@@ -1,9 +1,13 @@
 package springbootpractice.kmg.springbootfirstpractice.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
 import java.time.LocalDateTime;
 
 public record Content(
         Integer id,
+        @NotBlank
         String title,
         String desc,
         Status status,
