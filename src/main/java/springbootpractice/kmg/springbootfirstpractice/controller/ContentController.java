47cab5259f2ef9ad7fe6565e89f785649ca8da94
Khaled Saleh <khaled@connou.app>
2023-05-10 16:35:35 +0200

    writing the CRUD operations

---
+++ src/main/java/springbootpractice/kmg/springbootfirstpractice/controller/ContentController.java
@@ -0,0 +1,50 @@
+package springbootpractice.kmg.springbootfirstpractice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import springbootpractice.kmg.springbootfirstpractice.model.Content;
+import springbootpractice.kmg.springbootfirstpractice.repository.ContentCollectionRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/content")
+@CrossOrigin
+public class ContentController {
+    private final ContentCollectionRepository repository;
+    public ContentController(ContentCollectionRepository repository){
+        this.repository = repository;
+    }
+    // make a request and find all the pieces of content in the system
+    @GetMapping("")
+    public List<Content> findAll(){
+        return repository.finalAll();
+    }
+    @GetMapping("/{id}")
+    public Content findById(@PathVariable Integer id){
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public void create(@RequestBody Content content){
+        repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@RequestBody Content content, @PathVariable Integer id){
+        if(!repository.existsById(id)){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found!");
+        }
+        repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id){
+        repository.delete(id);
+    }
+}
--- src/main/java/springbootpractice/kmg/springbootfirstpractice/repository/ContentCollectionRepository.java
+++ src/main/java/springbootpractice/kmg/springbootfirstpractice/repository/ContentCollectionRepository.java
@@ -1,23 +1,52 @@
 package springbootpractice.kmg.springbootfirstpractice.repository;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 import springbootpractice.kmg.springbootfirstpractice.model.Content;
+import springbootpractice.kmg.springbootfirstpractice.model.Status;
+import springbootpractice.kmg.springbootfirstpractice.model.Type;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public class ContentCollectionRepository {
-    private final List<Content> content = new ArrayList<>();
+    private final List<Content> contentList = new ArrayList<>();
 
     public ContentCollectionRepository(){
 
     }
     public List<Content> finalAll(){
-        return content;
+        return contentList;
     }
     public Optional<Content> findById(Integer id){
-        return content.stream().filter(c -> c.id().equals(id)).findFirst();
+        return contentList.stream().filter(c -> c.id().equals(id)).findFirst();
+    }
+    public void save(Content content) {
+        contentList.removeIf(c -> c.id().equals(content.id()));
+        contentList.add(content);
+    }
+    @PostConstruct
+    private void init(){
+        Content content  = new Content(
+                1,
+                "This is my first blog post",
+                "My first blog post",
+                Status.IDEA,
+                Type.ARTICLE,
+                LocalDateTime.now(),
+                null,
+                "");
+        contentList.add(content);
+    }
+
+    public boolean existsById(Integer id) {
+        return contentList.stream().filter(c -> c.id().equals(id)).count() == 1;
+    }
+
+    public void delete(Integer id) {
+        contentList.removeIf(c -> c.id().equals(id));
     }
 }
