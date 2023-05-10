511eca34d1920d047d6106399a56e9c22d007890
Khaled Saleh <khaled@connou.app>
2023-05-10 17:56:23 +0200

    adding a CommandLineRunner to the main class to add an element to our database on running the app

--- src/main/java/springbootpractice/kmg/springbootfirstpractice/Application.java
+++ src/main/java/springbootpractice/kmg/springbootfirstpractice/Application.java
@@ -1,7 +1,15 @@
 package springbootpractice.kmg.springbootfirstpractice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springbootpractice.kmg.springbootfirstpractice.model.Content;
+import springbootpractice.kmg.springbootfirstpractice.model.Status;
+import springbootpractice.kmg.springbootfirstpractice.model.Type;
+import springbootpractice.kmg.springbootfirstpractice.repository.ContentRepository;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +18,19 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(ContentRepository repository){
+		return args -> {
+			Content content  = new Content(
+					null,
+					"you are watching Khaled practicing Spring boot",
+					"Hope you learn something new",
+					Status.IDEA,
+					Type.COURSE,
+					LocalDateTime.now(),
+					null,
+					"");
+			repository.save(content);
+		};
+	}
 }
